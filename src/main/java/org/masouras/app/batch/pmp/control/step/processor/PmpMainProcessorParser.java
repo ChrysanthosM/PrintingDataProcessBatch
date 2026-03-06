f7364dc3d3d8a5b9479f45369dc80f7aadae87b7
ChrysanthosM <ChrysanthosM@Outlook.com>
2026-03-06 19:48:29 +0200

    commit

--- src/main/java/org/masouras/app/batch/pmp/control/step/processor/PmpMainProcessorParser.java
+++ src/main/java/org/masouras/app/batch/pmp/control/step/processor/PmpMainProcessorParser.java
@@ -3,6 +3,8 @@ package org.masouras.app.batch.pmp.control.step.processor;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.masouras.app.batch.pmp.control.step.business.MainProcessParserService;
+import org.masouras.app.rabbit.business.printing.model.PrintJobMessage;
+import org.masouras.app.rabbit.business.printing.producer.PrintingJobProducer;
 import org.masouras.model.mssql.schema.jpa.control.entity.PrintingDataEntity;
 import org.springframework.batch.infrastructure.item.ItemProcessor;
 import org.springframework.stereotype.Component;
@@ -11,10 +13,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class PmpMainProcessorParser implements ItemProcessor<PrintingDataEntity, PrintingDataEntity> {
     private final MainProcessParserService mainProcessParserService;
+    private final PrintingJobProducer printingJobProducer;
 
     @Override
     public PrintingDataEntity process(@NotNull PrintingDataEntity printingDataEntity) {
-        return mainProcessParserService.processPrintingDataEntity(printingDataEntity);
+        switch (printingDataEntity.getPrintingWayType()) {
+            case BATCH -> {
+                return mainProcessParserService.processPrintingDataEntity(printingDataEntity);
+            }
+            case RABBIT -> {
+                printingJobProducer.sendMediumPriority(new PrintJobMessage(printingDataEntity.getId()));
+                return printingDataEntity;
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + printingDataEntity.getPrintingWayType());
+        }
     }
 }
 
